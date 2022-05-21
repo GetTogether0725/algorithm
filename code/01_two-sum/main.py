540dc4d5207cf427a3e803d416a8a0eb8b747528
Gonglja <glj0@outlook.com>
2022-05-21 15:33:03 +0800

    优化模板

--- code/01_two-sum/main.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(0,len(nums),1):
-            v = target-nums[i]
-            if v in nums:
-                if i!=nums.index(v):
-                    return[i,nums.index(v)]
